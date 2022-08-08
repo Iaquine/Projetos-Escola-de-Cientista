a43c19a333da49c79c84128578e41ffc8ce1e194
Iaquine <61883817+Iaquine@users.noreply.github.com>
2022-08-08 14:27:36 -0300

    Modificações no serviço principal

--- Projeto 1/core-swagger-api/service/service/main_service.py
+++ Projeto 1/core-swagger-api/service/service/main_service.py
@@ -1,11 +1,11 @@
+from imp import load_module
+from pyexpat import model
 import time
 import json
 from loguru import logger
 from service.constants import mensagens
 import pandas as pd
-from pickle import dump, load
-#from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
+import pickle as pck
 
 class shows():
 
@@ -17,52 +17,50 @@ class shows():
         """"
         Carrega o modelo de classificacão de shows a ser usado
         """
-        filename = '/Users/iaquine/Documents/GitHub/Projetos-Escola-de-Cientista/Projeto 1/arvore_model.h5'
-        self.model = load(open(filename, 'rb'))
-        print('Deu certo')
+        filename = '/code/arvore_model2.pkl'
+        self.model = pck.load(open(filename, 'rb'))
         logger.debug(mensagens.FIM_LOAD_MODEL)
 
-    def executar_rest(self, texts):
+        return model
+
+    def executar_rest(self, band_scores):
         response = {}
 
         logger.debug(mensagens.INICIO_PREDICT)
         start_time = time.time()
 
-        response_predicts = self.buscar_predicao(texts['textoMensagem'])
+        response_predicts = self.buscar_predicao(band_scores['textoMensagem'])
 
         logger.debug(mensagens.FIM_PREDICT)
         logger.debug(f"Fim de todas as predições em {time.time()-start_time}")
-
-        df_response = pd.DataFrame(texts, columns=['textoMensagem'])
+        
+        df_response = pd.DataFrame(band_scores, columns=['textoMensagem'])
         df_response['predict'] = response_predicts
 
         df_response = df_response.drop(columns=['textoMensagem'])
+        df_response = df_response.head(1)
 
         response = {
-                     "listaClassificacoes": json.loads(df_response.to_json(
+                     "Classificação": json.loads(df_response.to_json(
                                                                             orient='records', force_ascii=False))}
 
         return response
 
-    def buscar_predicao(self, texts):
+    def buscar_predicao(self, band_scores):
         """
         Pega o modelo carregado e aplica em texts
         """
         logger.debug('Iniciando o predict...')
 
-        response = []
+        model = self.load_model()
+        #model.compile()
+        predicted = model.predict([band_scores])
 
-        for text in texts:
-            sentiment_dict = self.model.polarity_scores(text)
+        if predicted == 0:
+            print(predicted)
+            return 'No! You should not go to this concert'
+        else:
+            print(predicted)
+            return 'YES! You should go to this concert'
 
-            # decide sentiment as positive, negative and neutral
-            if sentiment_dict['compound'] >= 0.05:
-                response.append("Positive")
-
-            elif sentiment_dict['compound'] <= - 0.05:
-                response.append("Negative")
-
-            else:
-                response.append("Neutral")
-
-        return response
+        
